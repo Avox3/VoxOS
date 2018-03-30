9043abd46aaebf6f29163342db2b5a2b86bf6bc0
Alon Vox <alonvox3@gmail.com>
2018-03-31 00:19:09 +0300

    Enable A20 line and load GDT

--- src/boot/Stage2/Stage2.asm
+++ src/boot/Stage2/Stage2.asm
@@ -7,24 +7,46 @@ org 0x0000
 jmp stage2	; jump to main
 
 stage2:
-			; Set DS = CS
-			mov ax, cs
-			mov ds, ax
 
-			mov si, Msg
-			call Print
+	; Set DS = CS
+	mov ax, cs
+	mov ds, ax
 
-			cli		; clear interrupts to prevent triple faults
-			hlt		; hault the system
+	cli	; clear interrupts
+
+	mov	ax, 0x9000 ; stack begins at 0x9000-0xffff
+	mov	ss, ax
+	mov	sp, 0xFFFF
+	sti ; enable interrupts
+
+	; print loading message
+	mov si, LoadMsg
+	call Print
+
+	; load global descriptor table
+	call install_GDT
+
+	cli	; clear interrupts
+	mov	eax, cr0	; set bit 0 in cr0 - enter pmode
+	or	eax, 1
+	mov	cr0, eax
+
+	; enable A20
+	call enable_A20
+
+	cli		; clear interrupts to prevent triple faults
+	hlt		; hault the system
 
 ;*************************************************;
 ;	Includes Section
 ;************************************************;
 
 %include 'src/boot/Others/utils.inc'
+%include 'src/boot/Stage2/A20.inc'
+%include 'src/boot/Stage2/Gdt.inc'
 
 ;*************************************************;
 ;	Data Section
 ;************************************************;
-
-Msg	db	13, 10, "Preparing to load operating system...",13, 10, 0
+A20_success	db	13, 10, "Enabled A20 successfully...",13, 10, 0
+LoadMsg	db	13, 10, "Preparing to load operating system...",13, 10, 0
